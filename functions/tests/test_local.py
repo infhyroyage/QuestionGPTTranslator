9d555595033fb9761a2e4a975818ff02670af435
Takeru KIDO <keidy2463@gmail.com>
2024-10-24 00:54:10 +0900

    Fix disabling W0613

--- functions/tests/test_local.py
+++ functions/tests/test_local.py
@@ -40,7 +40,11 @@ class TestLocalUtils(unittest.TestCase):
     @patch("util.local.os.listdir")
     @patch("util.local.os.path.isdir")
     def test_create_import_data_when_data_exists(
-        self, mock_isdir, mock_listdir, mock_exists, mock_builtins_open
+        self,
+        mock_isdir,
+        mock_listdir,
+        mock_exists,
+        mock_builtins_open,  # pylint: disable=W0613
     ):
         """dataファイル/ディレクトリが存在する場合のcreate_import_data関数のテスト"""
 
